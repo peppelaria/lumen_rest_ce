e33480b5b7ead71f3e7694fdca9e65b6a111f405
peppelaria <peppelaria@gmail.com>
2016-05-19 14:04:55 +0200

    testApi

---
+++ app/Http/Controllers/TestListController.php
@@ -0,0 +1,71 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use App\Models\Test;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+  
+  
+class TestListController extends Controller{
+	public function index(){
+  
+        $Tests  = test::all();
+  
+        return response()->json($Tests);
+  
+    }
+  
+    public function getTest($cf_id){
+  
+        $test  = Test::find($cf_id);
+  
+        return response()->json($test);
+    }
+  
+    public function createTest(Request $request){
+  
+        $Test = Test::create($request->all());
+  
+        return response()->json($Test);
+  
+    }
+  
+    public function deleteTest($cf_id){
+        $Test  = Test::find($cf_id);
+        $Test->delete();
+ 
+        return response()->json('deleted');
+    }
+  
+    public function updateTest(Request $request,$cf_id){
+        $Test  = Test::find($cf_id);
+        $Test->cf_uid = $request->input('cf_uid');
+        $Test->cf_created = $request->input('cf_created');
+        $Test->cf_modified = $request->input('cf_modified');
+        $Test->cf_created_by = $request->input('cf_created_by');
+        $Test->cf_modified_by = $request->input('cf_modified_by');
+        $Test->cf_ipaddress = $request->input('cf_ipaddress');
+        $Test->test_uid = $request->input('test_uid');
+        $Test->test_id = $request->input('test_id');
+        $Test->start_available_date = $request->input('start_available_date');
+        $Test->end_available_date = $request->input('end_available_date');
+        $Test->student_id = $request->input('student_id');
+        $Test->course_id = $request->input('course_id');
+        $Test->test_questions = $request->input('test_questions');
+        $Test->start_time = $request->input('start_time');
+        $Test->end_time = $request->input('end_time');
+        $Test->test_started = $request->input('test_started');
+        $Test->test_done = $request->input('test_done');
+        $Test->time_lock = $request->input('time_lock');
+        $Test->printed = $request->input('printed');
+        $Test->print_date = $request->input('print_date');
+        $Test->recycle = $request->input('recycle');
+        $Test->save();
+  
+        return response()->json($Test);
+    }
+
+ 
+}
+?>
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,21 +12,32 @@
 */
 
 $app->get('/', function() use ($app) {
-    return "Lumen RESTful API By CoderExample (http://coderexample.com)";
+	return "Lumen RESTful API By CoderExample (http://coderexample.com)";
 });
- 
+
 $app->group(['prefix' => 'api/v1','namespace' => 'App\Http\Controllers'], function($app)
 {
 
     //user
 
-    $app->get('user','UserController@index');
-  
-    $app->get('user/{id}','UserController@getUser');
-      
-    $app->post('user','UserController@createUser');
-      
-    $app->put('user/{id}','UserController@updateUser');
-      
-    $app->delete('user/{id}','UserController@deleteUser');
+	$app->get('user','UserController@index');
+
+	$app->get('user/{id}','UserController@getUser');
+
+	$app->post('user','UserController@createUser');
+
+	$app->put('user/{id}','UserController@updateUser');
+
+	$app->delete('user/{id}','UserController@deleteUser');
+
+    //Tests
+	$app->get('test','TestListController@index');
+
+	$app->get('test/{cf_id}','TestListController@getTest');
+
+	$app->post('test','TestListController@createTest');
+
+	$app->put('test/{cf_id}','TestListController@updateTest');
+
+	$app->delete('test/{cf_id}','TestListController@deleteTest');
 });
